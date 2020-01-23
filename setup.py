c0a06772427781754f8ce9c4ce0307e16cd9e977
Chris <60240205+Scissorbot@users.noreply.github.com>
2020-01-23 13:58:41 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,3 @@
+#Folder structures are of the form:
+# Root (media folder) > Model (camera type) > Camera Label > Card > Media Files
+dir_in = "Z:/01_ACTIVE_PROJECTS/1367_WilsonDow_Tepro/20 FOOTAGE/21 SOURCE/Halo"      #Root of directory in
